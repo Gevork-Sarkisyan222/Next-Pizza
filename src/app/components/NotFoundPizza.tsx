42ec76d296e281eb3118a0c1724de40bd5271a47
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2023-11-08 12:26:08 +0400

    adding changes

--- src/app/components/NotFoundPizza.tsx
+++ src/app/components/NotFoundPizza.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
+import { useSelector } from 'react-redux';
 
 function NotFoundPizza() {
+  const theme = useSelector((state: any) => state.changeTheme.theme);
+
   return (
     <div className="not-found-content" style={{ textAlign: 'center' }}>
-      <h1>По вашему запросу ничего не найдено 😔</h1>
+      <h1 className={`${theme ? 'black-not-found-text' : ''}`}>
+        По вашему запросу ничего не найдено 😔
+      </h1>
       <Image
         width={600}
         height={500}
