36fea04249c160a30426b9514f8848441ea33fbe
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2023-11-27 19:04:28 +0400

    fixing error with components

--- src/app/redux/slices/selectedAvatar.slice.ts
+++ src/app/redux/slices/selectedAvatar.slice.ts
@@ -1,10 +1,8 @@
+'use client';
+
 import { createSlice } from '@reduxjs/toolkit';
 
-let savedAvatar = null;
-
-if (typeof window !== 'undefined') {
-  savedAvatar = localStorage.getItem('selectedAvatar');
-}
+const savedAvatar = localStorage.getItem('selectedAvatar');
 
 const initialState = {
   selectedAvatar:
@@ -12,23 +10,18 @@ const initialState = {
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8PxtAWTgOyp0m_7NgdCm3T_9-aU0Zhg47SvX-AaLTU4y0kEvuk-maQdJeTNadSg3rFi0&usqp=CAU',
 };
 
-// Ваш срез Redux
 const selectedAvatar = createSlice({
   name: 'selected/avatar',
   initialState,
   reducers: {
     setSelectedAvatar: (state, action) => {
       state.selectedAvatar = action.payload;
-      if (typeof window !== 'undefined') {
-        localStorage.setItem('selectedAvatar', action.payload);
-      }
+      localStorage.setItem('selectedAvatar', action.payload);
     },
     clearAvatar: (state) => {
       state.selectedAvatar =
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8PxtAWTgOyp0m_7NgdCm3T_9-aU0Zhg47SvX-AaLTU4y0kEvuk-maQdJeTNadSg3rFi0&usqp=CAU';
-      if (typeof window !== 'undefined') {
-        localStorage.removeItem('selectedAvatar');
-      }
+      localStorage.removeItem('selectedAvatar');
     },
   },
 });
